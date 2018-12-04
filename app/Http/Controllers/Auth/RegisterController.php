13b3c47900f2619e01afe395ec0efc0ad88ceee7
abhinavreddygodhala <44847942+abhinavreddygodhala@users.noreply.github.com>
2018-12-04 00:52:35 -0500

    cognito authentication support

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -47,7 +47,7 @@ class RegisterController extends Controller
      * @param  array  $data
      * @return \Illuminate\Contracts\Validation\Validator
      */
-    /**
+    
     protected function validator(array $data)
     {
         return Validator::make($data, [
@@ -63,7 +63,6 @@ class RegisterController extends Controller
      * @param  array  $data
      * @return \App\User
      */
-    /**
     protected function create(array $data)
     {
         return User::create([
@@ -71,5 +70,5 @@ class RegisterController extends Controller
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
-    }*/
+    }
 }
