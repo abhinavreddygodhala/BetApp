28332a9dc3f231e65a2091014f207a0357760cd4
abhinavreddygodhala <44847942+abhinavreddygodhala@users.noreply.github.com>
2018-12-05 18:56:45 -0500

    updated

--- resources/views/payment.blade.php
+++ resources/views/payment.blade.php
@@ -1,4 +1,27 @@
 @extends('layouts.app')
+@section('js')
+    <script>
+        var button = document.querySelector('#submit-button');
+
+        braintree.dropin.create({
+            authorization: "{{ Braintree_ClientToken::generate() }}",
+            container: '#dropin-container'
+        }, function (createErr, instance) {
+            button.addEventListener('click', function () {
+                instance.requestPaymentMethod(function (err, payload) {
+                    $.get('{{ route('payments.process') }}', {payload}, function (response) {
+                        if (response.success) {
+                            alert('Payment successfull!');
+                        } else {
+                            alert('Payment failed');
+                        }
+                    }, 'json');
+                });
+            });
+        });
+    </script>
+@endsection
+
 @section('content')
 <div class="container">
     <div class="row">
@@ -8,24 +31,5 @@
         </div>
     </div>
 </div>
-<script>
-    var button = document.querySelector('#submit-button');
 
-    braintree.dropin.create({
-        authorization: "{{ Braintree_ClientToken::generate() }}",
-        container: '#dropin-container'
-    }, function (createErr, instance) {
-        button.addEventListener('click', function () {
-            instance.requestPaymentMethod(function (err, payload) {
-                $.get('{{ route('payments.process') }}', {payload}, function (response) {
-                    if (response.success) {
-                        alert('Payment successfull!');
-                    } else {
-                        alert('Payment failed');
-                    }
-                }, 'json');
-            });
-        });
-    });
-</script>
 @endsection
